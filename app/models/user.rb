ae0db0589ef839089b32f1eead9cf0a169ec48df
gheloshun <agdavid112191@gmail.com>
2020-01-06 17:58:22 +0800

    Add authenticated method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
   has_one_attached :image
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :first_name, presence: true, length: { maximum: 50 }
@@ -10,28 +9,20 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-  BCrypt::Password.create(string, cost: cost)
-end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def display_image
     image.variant(resize_to_limit: [100, 100])
   end
 
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+  def display_avatar
+    image.variant(resize_to_limit: [200, 200])
   end
 
-  # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def forget
+    update_attribute(:remember_digest, nil  )
   end
 end
