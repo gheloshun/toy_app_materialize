a363aae50acd79a0b23f35ccaa789d9d35c5dcaa
gheloshun <agdavid112191@gmail.com>
2020-01-17 12:20:15 +0800

    Apply multiple image upload for micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,17 +1,17 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :images, content_type: { in: %w[image/jpeg image/gif image/png],
     message: "must be a valid image format"
   }, size: {
       less_than: 5.megabytes,
       message: "should be less than 5MB"
     }
 
-    def display_micropost_image
-      image.variant(resize_to_limit: [500, 500])
+    def display_micropost_image input
+      return self.images[input].variant(resize_to_limit: [250, 250])
     end
 end
