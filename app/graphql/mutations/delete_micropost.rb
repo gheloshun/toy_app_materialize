4890dc63c9787f659d944757535bc95095b3465c
gheloshun <agdavid112191@gmail.com>
2020-01-24 13:06:08 +0800

    Add delete micropost mutation

---
+++ app/graphql/mutations/delete_micropost.rb
@@ -0,0 +1,12 @@
+module Mutations
+  class DeleteMicropost < BaseMutation
+    argument :id, ID, required: true
+
+    field :micropost, Types::MicropostType, null: false
+
+    def resolve(id:)
+      micropost = Micropost.find_by_id(id).destroy
+      { micropost: micropost }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :update_user, mutation: Mutations::UpdateUser
     field :sign_in_user, mutation: Mutations::SignInUser
+    field :delete_micropost, mutation: Mutations::DeleteMicropost
   end
 end
